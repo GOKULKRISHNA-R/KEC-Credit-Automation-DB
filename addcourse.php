d59ceccc155b1883d9f23ba97f639b8f91a0c6e6
Gokulkrishna Raju <gmkrishna11082003@gmail.com>
2023-02-06 11:17:08 +0530

    Course Queries

---
+++ addcourse.php
@@ -0,0 +1,27 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+ 
+include 'DbConnect.php';
+$objDb = new DbConnect;
+$conn = $objDb->connect();
+ 
+$user = json_decode( file_get_contents('php://input') );
+
+$sql = "INSERT INTO college_courses(course_code,course_name,course_duration,course_credit,course_is_for) VALUES(:code, :name, :weeks, :credits, :year)";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':code', $user->code);
+$stmt->bindParam(':name', $user->name  );
+$stmt->bindParam(':weeks', $user->weeks );
+$stmt->bindParam(':credits', $user->credits );
+$stmt->bindParam(':year', $user->year );
+
+if($stmt->execute()) {
+    $response = ['status' => 1, 'message' => 'Record created successfully.'];
+} else {
+    $response = ['status' => 0, 'message' => 'Failed to create record.'];
+}
+echo json_encode($response);
---
+++ addnptelcourse.php
@@ -0,0 +1,30 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+ 
+include 'DbConnect.php';
+$objDb = new DbConnect;
+$conn = $objDb->connect();
+ 
+$user = json_decode( file_get_contents('php://input') );
+
+$sql = "INSERT INTO nptel_course(course_code,course_name,type_of_certificate,weeks,credits,assignment_score,exam_score,final_score,certificate) VALUES(null, :name, :type,:weeks, :credits,:assignment_score,:exam_score,:final_score,:proof)";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':name', $user->name  );
+$stmt->bindParam(':type', $user->type  );
+$stmt->bindParam(':weeks', $user->weeks );
+$stmt->bindParam(':credits', $user->credits );
+$stmt->bindParam(':assignment_score', $user->assignment_score );
+$stmt->bindParam(':exam_score', $user->exam_score );
+$stmt->bindParam(':final_score', $user->final_score );
+$stmt->bindParam(':proof', $user->proof );
+
+if($stmt->execute()) {
+    $response = ['status' => 1, 'message' => 'Record created successfully.'];
+} else {
+    $response = ['status' => 0, 'message' => 'Failed to create record.'];
+}
+echo json_encode($response);
