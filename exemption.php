426cb9866adda410bf4fd21112ba3b831c8b579d
Arunaa S <arunaasureshkumar@gmail.com>
2023-02-06 23:43:54 +0530

    Update exemption.php

--- exemption.php
+++ exemption.php
@@ -11,13 +11,17 @@ $conn = $objDb->connect();
  
 $user = json_decode( file_get_contents('php://input') );
 
-$sql = "INSERT INTO exemption(roll,semester,academic_course,credit_course) VALUES(:roll, :sem, :courses, :credit)";
+$sql = "INSERT INTO exemption(roll,semester,type_of_course,academic_course_code,academic_course_name,academic_course_credit,course_code,course_credit,course_name) VALUES(:roll, :sem, :type, :code,:name,:credit,:code1,:name1,:credit1)";
 $stmt = $conn->prepare($sql);
 $stmt->bindParam(':roll', $user->roll);
 $stmt->bindParam(':sem', $user->sem  );
-$stmt->bindParam(':courses', $user->courses );
+$stmt->bindParam(':type', $user->type );
+$stmt->bindParam(':code', $user->code );
+$stmt->bindParam(':name', $user->name );
 $stmt->bindParam(':credit', $user->credit );
-
+$stmt->bindParam(':code1', $user->code1 );
+$stmt->bindParam(':credit1', $user->credit1 );
+$stmt->bindParam(':name1', $user->name1 );
 if($stmt->execute()) {
     $response = ['status' => 1, 'message' => 'Record created successfully.'];
 } else {
